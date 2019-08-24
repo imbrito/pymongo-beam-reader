691603b07bc7aa350b61af77b417d1251b80db34
imbrito <ismael.20rj@gmail.com>
2019-08-24 18:11:01 -0300

    update consumer

--- change_streams/consumer.py
+++ change_streams/consumer.py
@@ -1,14 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from settings import client
-from bson.json_util import dumps
+from datetime import datetime
 
 
 if __name__ == "__main__":
 
     stream = client.get_database("changestreams").get_collection("profile").watch(full_document='updateLookup')
-    while stream:
-        document = next(stream)
-        print(document)
-        # print(dumps(document))
-        print('') # for readability only
+    try:
+        while stream:
+            document = stream.next()
+            print("{}\n".format(document))
+
+    except KeyboardInterrupt:
+            print("\n{} - finished by KeyboardInterrupt.".format(
+                datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ"),
+            ))
+    except Exception as excpetion:
+        print("\n{} - finished by Exception: {}.".format(
+            datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ"),
+            excpetion
+        ))
