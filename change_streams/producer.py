26a951abcd6e796b1943a470ddbb1ab2fbb91d39
imbrito <ismael.20rj@gmail.com>
2019-08-24 18:11:36 -0300

    update producer

--- change_streams/producer.py
+++ change_streams/producer.py
@@ -3,7 +3,10 @@
 from settings import client
 from faker import Faker
 from datetime import datetime
+from time import sleep
 import argparse
+
+agent = Faker()
 
 
 def check_positive(value):
@@ -15,28 +18,69 @@ def check_positive(value):
 
 def read_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-d", "--documents", type=check_positive, default=5, help="number of documents write to mongodb.")
+    parser.add_argument("-n", "--number", type=check_positive, default=5, help="number of documents write to mongodb.")
+    parser.add_argument("-i", "--insert", help="insert documents on mongodb.", action="store_true")
+    parser.add_argument("-u", "--update", help="update documents on mongodb.", action="store_true")
+    parser.add_argument("-d", "--delete", help="delete documents on mongodb.", action="store_true")
     return parser.parse_args()
 
 
+def logger(action, _id):
+    return print("{time} - {action} {id}.".format(
+            time=datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ"),
+            action=action,
+            id=_id
+        ))
+
+
+def insert_document(collection, number):
+    for x in range(number):
+        document = agent.profile()
+        document["current_location"] = [float(document["current_location"][0]),float(document["current_location"][1])]
+        document["birthdate"] = document["birthdate"].strftime("%Y-%m-%d") 
+        document["created"] = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+        collection.insert_one(document)
+        logger("inserted", document["_id"])
+
+
+def update_document(collection, number):
+    documents = collection.find(limit=number)
+    for document in documents:
+        collection.update_one({"_id": document["_id"]}, {"$set": {"job": agent.job()}})
+        logger("updated", document["_id"])
+
+
+def delete_document(collection, number):
+    documents = collection.find(limit=number)
+    for document in documents:
+        collection.delete_one({"_id": document["_id"]})
+        logger("deleted", document["_id"])
+
+
 if __name__ == "__main__":
+    
     args = read_args()
-    agent = Faker()
-    collection = client.get_database("changestreams").get_collection("profile")
-    for x in range(args.documents):
-        try:
-            profile = agent.profile()
-            profile["current_location"] = [float(profile["current_location"][0]),float(profile["current_location"][1])]
-            profile["birthdate"] = profile["birthdate"].strftime("%Y-%m-%d") 
-            profile["created"] = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ")
-            collection.insert_one(profile)
-            # client.changestreams.profile.insert_one(profile)
-            print("{}: {}.".format(
-                datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ"),
-                profile
-            ))
-        except KeyboardInterrupt:
-            print("\n{}: finished by KeyboardInterrupt.".format(
-                datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ"),
-            ))
-            break
+    
+    try:
+        collection = client.get_database("changestreams").get_collection("profile")
+        # collection = client.changestreams.profile
+
+        if args.insert:
+            insert_document(collection=collection, number=args.number)
+
+        if args.update:
+            update_document(collection=collection, number=args.number)
+            sleep(0.5)
+        
+        if args.delete:
+            delete_document(collection=collection, number=args.number)
+
+    except KeyboardInterrupt:
+        print("\n{} - finished by KeyboardInterrupt.".format(
+            datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ"),
+        ))
+    except Exception as excpetion:
+        print("\n{} - finished by Exception: {}.".format(
+            datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ"),
+            excpetion
+        ))
